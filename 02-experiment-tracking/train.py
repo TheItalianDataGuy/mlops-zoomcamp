5bd3cfee508de50ab9fe2e5bc735782bfd91ae6d
The Italian Data Guy <andreamarella0@gmail.com>
2025-05-19 21:01:17 +0100

    Completed question 4

--- 02-experiment-tracking/train.py
+++ 02-experiment-tracking/train.py
@@ -30,7 +30,7 @@ def run_train(data_path: str):
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
 
-        rmse = root_mean_squared_error(y_val, y_pred, squared=False)
+        rmse = root_mean_squared_error(y_val, y_pred)
 
 
 if __name__ == '__main__':
