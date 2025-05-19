18b9b3a9c2c2d30974ee2071be1c7010ad675bdc
Andrew <166432532+TheItalianDataGuy@users.noreply.github.com>
2025-05-19 18:44:45 +0000

    Implement MLflow autologging in training script and update experiment tracking metadata

--- 02-experiment-tracking/train.py
+++ 02-experiment-tracking/train.py
@@ -1,10 +1,13 @@
 import os
 import pickle
 import click
+import mlflow
 
+import mlflow.sklearn
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error
 
+mlflow.sklearn.autolog()
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -22,11 +25,12 @@ def run_train(data_path: str):
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    with mlflow.start_run():
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
-    rmse = root_mean_squared_error(y_val, y_pred, squared=False)
+        rmse = root_mean_squared_error(y_val, y_pred, squared=False)
 
 
 if __name__ == '__main__':
